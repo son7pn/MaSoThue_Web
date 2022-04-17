b1a6413c830d6f95ae1020c0ce9bc96d8ff9c86b
son7pn <son7pn@gmail.com>
2022-04-17 10:45:34 +0700

    add swiper

--- components/shared/Banner.vue
+++ components/shared/Banner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="banner">
-    <!-- <swiper
+    <swiper
       id="swiper-event-calendar"
       ref="carousel"
       class="swiper"
@@ -9,18 +9,18 @@
       <swiper-slide v-for="(item, index) of 6" :key="index">
         d
       </swiper-slide>
-    </swiper> -->
+    </swiper>
   </div>
 </template>
 
 <script>
-// import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-// import { Swiper as SwiperClass, Navigation, Pagination, Mousewheel, Autoplay, EffectFade } from 'swiper/swiper.esm'
-// SwiperClass.use([Navigation, Pagination, Mousewheel, Autoplay, EffectFade])
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import { Swiper as SwiperClass, Navigation, Pagination, Mousewheel, Autoplay, EffectFade } from 'swiper/swiper.esm'
+SwiperClass.use([Navigation, Pagination, Mousewheel, Autoplay, EffectFade])
 export default {
   components: {
-    // Swiper,
-    // SwiperSlide
+    Swiper,
+    SwiperSlide
   },
 
   data () {
